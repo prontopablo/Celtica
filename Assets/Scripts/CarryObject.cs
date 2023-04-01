6f9f3627b90f420a075e1d67a807c3fc365f4ee4
Darragh Nolan <55544101+prontopablo@users.noreply.github.com>
2023-04-01 20:03:21 +0200

    Update CarryObject.cs

--- Assets/Scripts/CarryObject.cs
+++ Assets/Scripts/CarryObject.cs
@@ -19,47 +19,54 @@ public class CarryObject : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<Collider2D>();
     }
-//
-void Update()
-{
-    // Check if the player wants to pick up an object
-    if (Input.GetMouseButtonDown(0) && !isCarrying)
-    {
-        // Cast a ray from the mouse position to see if there is an object to pick up
-        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0f, objectLayers);
 
-        if (hit.collider != null && Vector2.Distance(transform.position, hit.collider.transform.position) <= pickupRange && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+    void Update()
+    {
+        // Check if the player wants to pick up an object
+        if (Input.GetMouseButtonDown(0) && !isCarrying)
         {
-            // Pick up the object and set the player as its parent
-            carriedObject = hit.collider.gameObject;
-            carriedObject.transform.SetParent(transform);
-            isCarrying = true;
+            // Cast a ray from the mouse position to see if there is an object to pick up
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0f, objectLayers);
+
+            if (hit.collider != null && Vector2.Distance(transform.position, hit.collider.transform.position) <= pickupRange && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                // Pick up the object and set the player as its parent
+                carriedObject = hit.collider.gameObject;
+                carriedObject.transform.SetParent(transform);
+                isCarrying = true;
+            }
+        }
+        
+        // Check if the player wants to throw the object
+        else if (Input.GetMouseButtonDown(1) && isCarrying)
+        {
+            // Throw the object in the direction of the mouse pointer and remove the player as its parent
+            carriedObject.transform.SetParent(null);
+            carriedObject.GetComponent<Rigidbody2D>().velocity = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized * throwForce;
+            carriedObject = null;
+            isCarrying = false;
+        }
+
+        // Check if the player wants to drop the object
+        else if (Input.GetMouseButtonDown(0) && isCarrying)
+        {
+            // Drop the object and remove the player as its parent
+            carriedObject.transform.SetParent(null);
+            carriedObject = null;
+            isCarrying = false;
+        }
+
+        // Move the object in front of the player if they are carrying one
+        if (isCarrying)
+        {
+            if (carriedObject == null)
+            {
+                isCarrying = false;
+            }
+            else
+            {
+                carriedObject.transform.position = transform.position + (Vector3.right * carryDistance * transform.localScale.x);
+            }
         }
     }
-    
-    // Check if the player wants to throw the object
-    else if (Input.GetMouseButtonDown(1) && isCarrying)
-    {
-        // Throw the object in the direction of the mouse pointer and remove the player as its parent
-        carriedObject.transform.SetParent(null);
-        carriedObject.GetComponent<Rigidbody2D>().velocity = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized * throwForce;
-        carriedObject = null;
-        isCarrying = false;
-    }
-
-    // Check if the player wants to drop the object
-    else if (Input.GetMouseButtonDown(0) && isCarrying)
-    {
-        // Drop the object and remove the player as its parent
-        carriedObject.transform.SetParent(null);
-        carriedObject = null;
-        isCarrying = false;
-    }
-
-    // Move the object in front of the player if they are carrying one
-    if (isCarrying)
-    {
-        carriedObject.transform.position = transform.position + (Vector3.right * carryDistance * transform.localScale.x);
-    }
-}
 }

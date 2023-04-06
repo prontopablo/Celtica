7e41f0527b30caa68a2347ac66f196e5f13af76c
Darragh Nolan <55544101+prontopablo@users.noreply.github.com>
2023-04-06 13:43:35 +0200

    Added variable fire burst length

--- Assets/Scripts/FlameBurst.cs
+++ Assets/Scripts/FlameBurst.cs
@@ -5,19 +5,27 @@ public class FlameBurst : MonoBehaviour
     [SerializeField]
     private ParticleSystem flameParticle;
 
+    [SerializeField]
+    private float maxFireBurstLength = 1.0f;
+
+    private float currentFireBurstLength = 0.0f;
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
         {
             flameParticle.transform.position = transform.position;
             flameParticle.Play();
-            Invoke("StopFlameParticle", 0.5f);
         }
-    }
-
-    private void StopFlameParticle()
-    {
-        flameParticle.Stop();
-        //flameParticle.transform.position = transform.position;
+        else if (Input.GetKey(KeyCode.I) && currentFireBurstLength < maxFireBurstLength)
+        {
+            currentFireBurstLength += Time.deltaTime;
+        }
+        else if (Input.GetKeyUp(KeyCode.I))
+        {
+            flameParticle.Stop();
+            flameParticle.transform.position = transform.position;
+            currentFireBurstLength = 0.0f;
+        }
     }
 }

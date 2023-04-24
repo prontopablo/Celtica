6981202a63660b1983d9733aa4be56e4482a0816
Darragh Nolan <55544101+prontopablo@users.noreply.github.com>
2023-04-24 23:13:25 +0200

    Adding new input system (gamepads, keyboard)
    This will make supporting local coop easier

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,19 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
-    public float digDistance = 1f;
-    public float crouchHeightModifier = 0.5f;
-    public float crouchSpeed = 2f;
     public float jumpTime = 0.25f; // Time to reach the apex of the jump
     public float variableJumpHeightMultiplier = 0.25f; // How much to multiply jump force by when jump button is released early
     public float fallSpeedMultiplier = 3f; // Multiplier for the player's fall speed  
     public float maxFallSpeed = 15f;
-    public KeyCode crouchKey = KeyCode.C;
     public LayerMask groundLayers;
 
 
@@ -21,11 +19,12 @@ public class PlayerController : MonoBehaviour
     private CapsuleCollider2D coll;
     private Vector2 standingColliderSize;
     private Vector2 standingColliderOffset;
+    private PlayerControls playerControls;
+    private InputAction movement;
 
     private float horizontalMove = 0f;
     private float coyoteTime = 0.2f;
     private bool isJumping = false;
-    private bool isCrouching = false;
     private float jumpTimeCounter;
 
     void Start()
@@ -34,10 +33,36 @@ public class PlayerController : MonoBehaviour
         coll = GetComponent<CapsuleCollider2D>();
         standingColliderSize = coll.size;
         standingColliderOffset = coll.offset;
+        playerControls = new PlayerControls();
     }
 
-        void Update()
+    private void OnEnable()
+    {
+        movement = playerControls.Player.Movement;
+        movement.Enable();
+
+        playerControls.Player.Jump.performed += DoJump;
+        playerControls.Player.Jump.Enable();
+    }
+
+    private void DoJump(InputAction.CallbackContext obj)
+    {
+        if (IsGrounded() || coyoteTime > 0f)
         {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+        playerControls.Player.Jump.Disable();
+    }
+    
+    void Update()
+    {
             if (IsGrounded())
             {
                 coyoteTime = 0.1f; // Reset coyoteTime when player is on the ground
@@ -48,7 +73,7 @@ public class PlayerController : MonoBehaviour
             }
 
             // Handle player movement
-            horizontalMove = Input.GetAxisRaw("Horizontal") * (Input.GetKey(crouchKey) ? crouchSpeed : moveSpeed);
+            horizontalMove = movement.ReadValue<Vector2>().x * moveSpeed;
 
             // Flip the player sprite depending on mouse position
             if (Input.mousePosition.x < Screen.width / 2)
@@ -60,15 +85,7 @@ public class PlayerController : MonoBehaviour
                 transform.localScale = new Vector2(1f, 1f);
             }
 
-            // Handle player jumping
-            if (Input.GetButtonDown("Jump") && (IsGrounded() || coyoteTime > 0f))
-            {
-                isJumping = true;
-                jumpTimeCounter = jumpTime;
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            }
-
-            if (Input.GetButton("Jump") && isJumping)
+            if (playerControls.Player.Jump.triggered && isJumping)
             {
                 if (jumpTimeCounter > 0)
                     {
@@ -77,41 +94,22 @@ public class PlayerController : MonoBehaviour
                     }
             }
 
-            if (Input.GetButtonUp("Jump"))
+            if (playerControls.Player.Jump.ReadValue<float>() > 0)
             {
-            isJumping = false;
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
-            }
-
-            // Handle player digging
-            if (Input.GetButtonDown("Dig"))
-            {
-                Debug.Log("Digging!");
-                Dig();
-            }
-
-            // Handle player crouching
-            isCrouching = Input.GetKey(KeyCode.C);
-
-            if (isCrouching)
-            {
-                coll.size = new Vector2(coll.size.x, standingColliderSize.y * crouchHeightModifier);
-                coll.offset = new Vector2(coll.offset.x, standingColliderOffset.y - ((standingColliderSize.y - coll.size.y) / 2f));
-            }
-            else
-            {
-                coll.size = standingColliderSize;
-                coll.offset = standingColliderOffset;
+                isJumping = false;
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
             }
 
             // Print the player speed
             //float speedInKph = rb.velocity.magnitude * 3.6f; // Convert from m/s to km/h
             //string speedString = speedInKph.ToString("0.00") + " km/h";
             //Debug.Log("Player speed: " + speedString);
-        }
+    }
 
-        void FixedUpdate()
-        {
+    void FixedUpdate()
+    {
+           Debug.Log("Movement values: " + movement.ReadValue<Vector2>());
+           
             // Move the player
             rb.velocity = new Vector2(horizontalMove, rb.velocity.y);
 
@@ -130,18 +128,7 @@ public class PlayerController : MonoBehaviour
                 //caps max fall speed
                 rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
             }
-        }
-
-    void Dig()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, digDistance, groundLayers);
-
-        if (hit.collider != null)
-        {
-            Destroy(hit.collider.gameObject);
-        }
-    }
-    
+    }    
     // Check if the player is on the ground
     private bool IsGrounded()
     {

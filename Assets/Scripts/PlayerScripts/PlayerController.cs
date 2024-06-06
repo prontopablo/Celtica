c024ee1f41b75e9a052c7260ca7ec5a5208b04ac
Darragh Nolan <darraghnolan123@gmail.com>
2024-06-06 17:56:25 +0100

    Restarting progress! Got input system mostly working again

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, PlayerInput.IPlayerControlsActions
 {
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
@@ -17,85 +17,102 @@ public class PlayerController : MonoBehaviour
     private CapsuleCollider2D coll;
     private float coyoteTime = 0.2f;
 
+    private PlayerInput playerInput;
 
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<CapsuleCollider2D>();
+        playerInput = new PlayerInput();
     }
 
-    private void OnMovement(InputValue value)
+    void OnEnable()
     {
-        movementInputValue = value.Get<Vector2>();
-        Debug.Log(movementInputValue);
+        playerInput.PlayerControls.SetCallbacks(this);
+        playerInput.PlayerControls.Enable();
     }
- 
+
+    void OnDisable()
+    {
+        playerInput.PlayerControls.Disable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInputValue = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && (IsGrounded() || coyoteTime > 0f))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    public void OnButtonRegular(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Button pressed");
+        }
+    }
+
+    public void OnButtonHold(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Button held");
+        }
+    }
+
+    public void OnWall(InputAction.CallbackContext context)
+    {
+        // Implement this method if you need to handle the Wall action.
+        // If you don't need it, you can leave it empty.
+    }
+
     void Update()
     {
-            if (IsGrounded())
-            {
-                coyoteTime = 0.1f; // Reset coyoteTime when player is on the ground
-            }
-            else
-            {
-                coyoteTime -= Time.deltaTime; // Subtract coyoteTime every frame player is not on the ground
-            }
+        if (IsGrounded())
+        {
+            coyoteTime = 0.1f; // Reset coyoteTime when player is on the ground
+        }
+        else
+        {
+            coyoteTime -= Time.deltaTime; // Subtract coyoteTime every frame player is not on the ground
+        }
 
-            // Flip the player sprite depending on horizontal movement
-            if (movementInputValue.x < 0)
-            {
-                transform.localScale = new Vector2(-1f, 1f);
-            }
-            else if (movementInputValue.x > 0)
-            {
-                transform.localScale = new Vector2(1f, 1f);
-            }
-
-            // Print the player speed
-            //float speedInKph = rb.velocity.magnitude * 3.6f; // Convert from m/s to km/h
-            //string speedString = speedInKph.ToString("0.00") + " km/h";
-            //Debug.Log("Player speed: " + speedString);
+        // Flip the player sprite depending on horizontal movement
+        if (movementInputValue.x < 0)
+        {
+            transform.localScale = new Vector2(-1f, 1f);
+        }
+        else if (movementInputValue.x > 0)
+        {
+            transform.localScale = new Vector2(1f, 1f);
+        }
     }
 
     void FixedUpdate()
     {
-            MoveLogic();
-            // Apply fall speed multiplier
-            if (rb.velocity.y < 0)
-            {
-                //fall faster
-                rb.velocity += Vector2.up * Physics2D.gravity.y * (fallSpeedMultiplier - 1) * Time.deltaTime;
-                //caps max fall speed
-                rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
-            }
-    }    
-
-    private void OnButtonRegular()
-    {
-        Debug.Log("Button pressed");
-    }
-    
-    private void OnButtonHold()
-    {
-        Debug.Log("Button held");
+        MoveLogic();
+        // Apply fall speed multiplier
+        if (rb.velocity.y < 0)
+        {
+            //fall faster
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallSpeedMultiplier - 1) * Time.deltaTime;
+            //caps max fall speed
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
+        }
     }
 
     private void MoveLogic()
     {
         Vector2 horizontalMovement = new Vector2(movementInputValue.x, 0f);
-        float horizontalVelocity = horizontalMovement.x * moveSpeed * Time.fixedDeltaTime;
+        float horizontalVelocity = horizontalMovement.x * moveSpeed;
 
-        if (movementInputValue.y > 0.5) // treat up movement as a jump
-        {
-            if(IsGrounded() || coyoteTime > 0f)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            }
-        }
-        else
-        {
-            rb.velocity = new Vector2(horizontalVelocity, rb.velocity.y); // horizontal movement
-        }
+        rb.velocity = new Vector2(horizontalVelocity, rb.velocity.y); // horizontal movement
     }
 
     // Check if the player is on the ground
